a94cca9c59f7a3c655221a63373496ebc3f28c64
Ramiro Visca <ramiro.visca@jamcity.com>
2022-09-14 20:37:45 -0300

    LoadBalanced FeignClient

--- clients/src/main/kotlin/com/blackboxdynamics/clients/fraud/FraudClient.kt
+++ clients/src/main/kotlin/com/blackboxdynamics/clients/fraud/FraudClient.kt
@@ -1,5 +1,6 @@
 package com.blackboxdynamics.clients.fraud
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable
     "fraud"
 )
 interface FraudClient {
-
+    @LoadBalanced
     @GetMapping(  "api/v1/frauds/{customerId}")
     fun isFraud(@PathVariable customerId:Long):Boolean
 }
--- customer/src/main/kotlin/com/blackboxdynamics/customer/config/CustomerConfig.kt
+++ customer/src/main/kotlin/com/blackboxdynamics/customer/config/CustomerConfig.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
 
+@Deprecated("Use Feign Client Instead")
 @Configuration
 class CustomerConfig {
 
